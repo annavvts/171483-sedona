7f941df0276d146b8b011ec1608087e955091be5
Anna Smirnova <ppomexi@gmail.com>
2016-05-13 18:50:59 +0300

    Исправления декоративных элементов

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var button = document.querySelector(".hotel-search");
+
+var element = document.querySelector(".hotel-form");
+
+button.addEventListener('click', function(){
+if (element.classList.contains("hotel-form-show")){
+element.classList.remove("hotel-form-show");
+} else {
+element.classList.add("hotel-form-show");
+}
+});
