3080893d9fdf096f8aca60d6014c64e6b226fb2d
Anna Smirnova <ppomexi@gmail.com>
2016-05-15 15:47:38 +0300

    Исправление формы

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ var button = document.querySelector(".hotel-search");
 var element = document.querySelector(".hotel-form");
 
 button.addEventListener('click', function(){
-if (element.classList.contains("hotel-form-show")){
-element.classList.remove("hotel-form-show");
-} else {
-element.classList.add("hotel-form-show");
-}
+  if (element.classList.contains("hotel-form-show")){
+    element.classList.remove("hotel-form-show");
+  } else {
+    element.classList.add("hotel-form-show");
+  }
 });
